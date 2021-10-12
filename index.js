08d1df0ff8d6e3b4734946e926fec3a8b5b9908f
mahmednabil109 <mahmednabil109@gmail.com>
2021-10-12 15:46:54 +0200

    fix: add cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -18,54 +18,63 @@ const GET_TEAMS = JSON.stringify({
 
 const GET_TEAMS_MEMBERS = JSON.stringify({
   "Yara ElSawaf": {
+    id: 12,
     name: "Yara ElSawaf",
     role: "UI Designer",
     goals_count: 5,
     goals_completed_count: 2,
   },
   "Hoda Hamdy": {
+    id: 25,
     name: "Hoda Hamdy",
     role: "UI Designer",
     goals_count: 5,
     goals_completed_count: 3,
   },
   "Nada Fadali": {
+    id: 78,
     name: "Nada Fadali",
     role: "UI Designer",
     goals_count: 9,
     goals_completed_count: 7,
   },
   "Yasmin Mourad": {
+    id: 97,
     name: "Yasmin Mourad",
     role: "UI Designer",
     goals_count: 10,
     goals_completed_count: 4,
   },
   "Ingie ElSaddawy": {
+    id: 55,
     name: "Ingie ElSaddawy",
     role: "UI Designer",
     goals_count: 6,
     goals_completed_count: 3,
   },
   "Nour Azab": {
+    id: 41,
     name: "Nour Azab",
     role: "UI Designer",
     goals_count: 5,
     goals_completed_count: 5,
   },
   "Reem Tarek": {
+    id: 49,
     name: "Reem Tarek",
     role: "UI Designer",
     goals_count: 4,
     goals_completed_count: 2,
   },
   "Mai ElMaghraby": {
+    id: 63,
     name: "Mai ElMaghraby",
     role: "UI Designer",
     goals_count: 7,
     goals_completed_count: 3,
   },
   "Abd ElWahab": {
+    id: 106,
     name: "Abd ElWahab",
     role: "UI Designer",
     goals_count: 6,
@@ -104,7 +113,7 @@ const GET_GOALS_WITH_SUBTASKS = JSON.stringify({
       name: "ad7r el standup",
       difficulty_points: 4,
       category: "hi",
-      timestamps: "Sun Oct 03 2021 21:10:38 GMT+0200",
+      timestamps: "Sun Feb 03 2021 21:10:38 GMT+0200",
       subtasks: [
         {
           id: 12,
@@ -129,7 +138,7 @@ const GET_GOALS_WITH_SUBTASKS = JSON.stringify({
       name: "try not to die",
       difficulty_points: 2,
       category: "Community",
-      timestamps: "Sun Oct 05 2021 21:10:38 GMT+0200",
+      timestamps: "Sun Dec 05 2021 21:10:38 GMT+0200",
       subtasks: [
         {
           id: 13,
@@ -147,14 +156,12 @@ const GET_GOALS_WITH_SUBTASKS = JSON.stringify({
 const GET_USER_INFO = JSON.stringify({
   id: 1,
   name: "noha",
-  firstName: "Hoda",
-  lastName: "Hamdy",
   email: "noha@robustastudio.com",
   started_at: "Sun Oct 03 2020 21:22:05 GMT+0200",
   position: 2,
   profile_photo: "link",
   timestamps: "Sun Oct 12 2020 21:22:05 GMT+0200",
-  role: "employee",
+  role: "manager",
   isRegistered: true,
 });
 
@@ -241,6 +248,6 @@ app.put("/user-info", (_, res) => {
   return res.send(POST_PUT_RES);
 });
 
-app.listen(process.env.PORT || 8000, (err) => {
-  console.log(`app is up and runnig on port ${process.env.PORT || 8000}`, err);
+app.listen(process.env.PORT || 8000, () => {
+  console.log("app is up and runnig on port 8000");
 });
